751114f9f6995229b8b85d9a05fb61d3e6eb73f0
Mr. Pineapple <mr.sad.pineapple@gmail.com>
2020-09-27 16:33:12 +0100

    Initial Commit
    Setup Workspace (Eclipse)
    Added main files

---
+++ src/main/java/co/uk/pinelogstudios/barrelsplus/BetterBarrels.java
@@ -0,0 +1,14 @@
+package co.uk.pinelogstudios.barrelsplus;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ *	Author:	Mr. Pineapple
+ */
+public class BetterBarrels implements ModInitializer {
+	
+	public static final String MOD_ID = "pinesbarrels";
+	
+	@Override
+	public void onInitialize() {}
+}
