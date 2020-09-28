41ff442a58ff86be571038886ad934a4d12db494
Mr. Pineapple <mr.sad.pineapple@gmail.com>
2020-09-28 01:32:45 +0100

    Fixed Block Item
    Added the Barrel to the Decoration's Creative Tab so it can be accessed in creative.

--- src/main/java/co/uk/pinelogstudios/barrelsplus/core/registry/BlockInit.java
+++ src/main/java/co/uk/pinelogstudios/barrelsplus/core/registry/BlockInit.java
@@ -5,6 +5,7 @@ import co.uk.pinelogstudios.barrelsplus.core.BetterBarrels;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -16,7 +17,7 @@ public class BlockInit {
 	public static final Block BARREL = registerBarrel(new BarrelBlock(), "better_barrel");
 	
 	private static Block registerBarrel(Block block, String name) {
-		Registry.register(Registry.ITEM, new Identifier(BetterBarrels.MOD_ID, name), new BlockItem(block, new FabricItemSettings().maxCount(1)));
+		Registry.register(Registry.ITEM, new Identifier(BetterBarrels.MOD_ID, name), new BlockItem(block, new FabricItemSettings().maxCount(1).group(ItemGroup.DECORATIONS)));
 		return Registry.register(Registry.BLOCK, new Identifier(BetterBarrels.MOD_ID, name), block);
 	}
 	
