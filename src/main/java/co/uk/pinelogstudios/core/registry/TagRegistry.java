51d960439700c21ba70fca930b84201da576ddc2
Mr-Pineapple <44590089+Mr-Pineapple@users.noreply.github.com>
2022-08-02 06:42:58 +0100

    :package: Updated Tags

--- src/main/java/co/uk/pinelogstudios/core/registry/TagRegistry.java
+++ src/main/java/co/uk/pinelogstudios/core/registry/TagRegistry.java
@@ -1,13 +1,18 @@
 package co.uk.pinelogstudios.core.registry;
 
 import co.uk.pinelogstudios.core.BetterBarrels;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.Tag;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 
 /**
  * Author: Mr. Pineapple
  */
 public class TagRegistry {
-    public static final Tag.Named<Block> FORBIDDEN_BARREL_CONTENTS = BlockTags.bind(BetterBarrels.MOD_ID + ":forbidden_barrel_contents");
+    private static TagKey<Item> createTag(String name) {
+        return TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(BetterBarrels.MOD_ID, name));
+    }
+
+    public static final TagKey<Item> FORBIDDEN_BARREL_CONTENT = createTag("forbidden_barrel_contents");
 }
